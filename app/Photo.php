3d68d47a22cd2934b426311100def91d638f2138
kota-es <info@k-3110.com>
2020-07-09 23:51:02 +0900

    写真取得API

--- app/Photo.php
+++ app/Photo.php
@@ -10,6 +10,21 @@ class Photo extends Model
     /** プライマリキーの型 */
     protected $keyType = 'string';
 
+    protected $appends = [
+        'url',
+    ];
+
+    // /** JSONに含めない属性 */
+    // protected $hidden = [
+    //     'user_id', 'filename',
+    //     self::CREATED_AT, self::UPDATED_AT,
+    // ];
+
+    /** JSONに含める属性 */
+    protected $visible = [
+        'id', 'owner', 'url',
+    ];
+
     /** IDの桁数 */
     const ID_LENGTH = 12;
 
@@ -51,5 +66,25 @@ class Photo extends Model
 
         return $id;
     }
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id', 'users');
+    }
+
+    /**
+     * アクセサ - url
+     * @return string
+     */
+    public function getUrlAttribute()
+    {
+        return Storage::cloud()->url($this->attributes['filename']);
+    }
+
+
 
 }
