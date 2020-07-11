5614dbb6c310e075019ebee398e1c612bfd362a0
kota-es <info@k-3110.com>
2020-07-11 12:22:58 +0900

    写真一覧ページ

--- resources/js/pages/PhotoList.vue
+++ resources/js/pages/PhotoList.vue
@@ -8,25 +8,40 @@
         :item="photo"
       />
     </div>
+    <pagination :current-page="currentPage" :last-page="lastPage" />
   </div>
 </template>
 
 <script>
 import { OK } from '../util'
 import Photo from '../components/Photo.vue'
+import Pagination from '../components/Pagination.vue'
 
 export default {
   components: {
-    Photo
+    Photo,
+    Pagination
+  },
+  props: {
+    page: {
+      type: Number,
+      required: false,
+      default: 1
+    }
   },
   data () {
     return {
-      photos: []
+      photos: [],
+      currentPage: 0,
+      lastPage: 0,
     }
+  },
+  created(){
+    console.log(this.$route)
   },
   methods: {
     async fetchPhotos () {
-      const response = await axios.get('/api/photos')
+      const response = await axios.get(`/api/photos/?page=${this.page}`)
 
       if (response.status !== OK) {
         this.$store.commit('error/setCode', response.status)
@@ -34,6 +49,8 @@ export default {
       }
 
       this.photos = response.data.data
+      this.currentPage = response.data.current_page
+      this.lastPage = response.data.last_page
     }
   },
   watch: {
